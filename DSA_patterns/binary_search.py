1fa645ead25ad238d3767cfc7b724a49c5b9d47c
Pfizer <lilkakaka224@gmail.com>
2026-07-07 01:46:12 +0700

    Create a binary search

---
+++ DSA_patterns/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(lst: list, item: int) -> int:
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = lst[mid]
+
+        if guess == item:
+            return guess
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1    
+    return None
+
